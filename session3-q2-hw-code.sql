65aec6f927103ed20d62e6545f57d1cd969633b4
Kaushik Bhadra <144113074+kauz-vii@users.noreply.github.com>
2025-12-15 10:24:13 +0530

    Add files via upload

---
+++ session3-q2-hw-code.sql
@@ -0,0 +1,5 @@
+select customer_id, count(customer_id) as count_no_trans
+from Visits where visit_id not in (
+select distinct(visit_id) from Transactions)
+group by customer_id;
+-- kaushik bhadra 23bai70483
